9eff15f087f24fe71cb722f458520d17a2e97094
Steven Dake <steve@computelify.com>
2023-06-18 01:50:13 -0700

    golden: debian 11 efi-based packer implementation
    Significant cleanup and verification of every preseed configuration
    
    This commit within the larger PR involved verifying every preseed configuration
    option using the documentation as a guide. The documentation is also referenced
    now within this code.
    
    This commit may not be precisely correct as I changed some ssh configuration. The
    real goal here though is to use a file provisioner and a shell provisioner to
    insert an overlay tar archive into the virtual machine and then untar it within
    the broader system.
    
    Co-Authored-by: Robert Starmer <robert@cmoputelify.com>

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# Set SSH password
-SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-# Pass SSH password and build time to packer
-PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} bullseye.pkr.hcl
+PACKER_LOG_PATH=$HOME/packer.log PACKER_LOG=1 packer build bullseye.pkr.hcl
