29225aa897a102cd10c82d066405690e04dbbe82
Steven Dake <steve@computelify.com>
2023-06-21 10:07:26 -0700

    Save results when a failure occurs

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ${PWD}/packer_logs
 
-PACKER_LOG_PATH=${PWD}/packer_logs/log.txt PACKER_LOG=1 packer build bullseye.pkr.hcl
+PACKER_LOG_PATH=${PWD}/packer_logs/log.txt PACKER_LOG=1 packer build -force -on-error=abort bullseye.pkr.hcl
