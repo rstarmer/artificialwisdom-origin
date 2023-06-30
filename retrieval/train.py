055959cad1c635b2553f8b359d975eaab6e931cf
Steven Dake <steve@computelify.com>
2023-06-30 10:16:36 -0700

    Train a retroformer
    The OneApi from Intel must be installed. The linker must be told about
    the oneapi libraries. I did this by dropping a file:
    ```
    sdake@beast-06:/etc/ld.so.conf.d$ cat artificial_wisdom_intel_one_api.conf
    /opt/intel/oneapi/mkl/2023.1.0/lib/intel64
    ```
    
    then rebuild the linker cache:
    ```
    sudo ldconfig
    ```
    
    - You may have to build faiss on your local system with GPU support.
    - The safety checks on the inputs could be better.
    - 100G appaers to break faiss.
    - Render the loss rate in the train loop.
    - Add the epoch to the saved name.
    - Move the dataloader to within the train loop.
    - requirements files are idpedently named.
    
    Change requirements file name.
    
    Remove the index_name parameter

--- retrieval/train.py
+++ retrieval/train.py
@@ -14,6 +14,7 @@ from rich.progress import (
     TimeElapsedColumn,
     TimeRemainingColumn,
 )
+<<<<<<< HEAD
 from retro_pytorch import RETRO, TrainingWrapper
 from rich.console import Console
 from rich.style import Style
@@ -47,6 +48,10 @@ theme = Theme(
 )
 
 console = Console(theme=theme)
+=======
+
+from retro_pytorch import RETRO, TrainingWrapper
+>>>>>>> 30f28ba (Train a retroformer)
 
 ###
 # instantiate RETRO, fit it into the TrainingWrapper with correct settings
